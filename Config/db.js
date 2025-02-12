75f183b35071a4c67f2091279f75a3936abb3e7a
latiep25 <144576955+latiep25@users.noreply.github.com>
2025-02-12 22:25:21 +0700

    Add files via upload

---
+++ Config/db.js
@@ -0,0 +1,10 @@
+// getting-started.js
+const mongoose = require('mongoose');
+
+const mongoDB = async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/mk3_b');
+  console.log('DB Connected')
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+module.exports = mongoDB
